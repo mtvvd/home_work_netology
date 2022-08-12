72b9a469d2009de5d3634558eecc4613186bb86e
Denis Matveev <matdenis99@yandex.ru>
2022-08-12 14:54:38 +0300

    Update hw_2.sql

--- hw_2.sql
+++ hw_2.sql
@@ -1,45 +1,48 @@
-create table if not exists musical_ganre (
-	musical_ganre_id int primary key,
-	name_of_the_ganre text not null
+CREATE TABLE IF NOT EXISTS musical_ganre (
+	musical_ganre_id INT PRIMARY KEY,
+	name_of_the_ganre VARCHAR(60) NOT NULL
 );
 
-create table if not exists performer (
-	performer_id int primary key,
-	performer_name text not null,
-	fk_musical_ganre_id int references musical_ganre(musical_ganre_id) not null
+CREATE TABLE IF NOT EXISTS performer (
+	performer_id INT PRIMARY KEY,
+	performer_name VARCHAR(60) NOT NULL
 );
 
-create table if not exists album (
-	album_id int primary key,
-	album_title text not null,
-	year_of_release date not null,
-	fk_performer_id int references performer(performer_id) not null
+CREATE TABLE IF NOT EXISTS album (
+	album_id INT PRIMARY KEY,
+	album_title VARCHAR(60) NOT NULL,
+	year_of_release DATE NOT NULL
 );
 
-create table if not exists music_track (
-	music_track_id int primary key,
-	track_name text not null,
-	track_duration time not null,
-	fk_album_id int references album(album_id) not null
+CREATE TABLE IF NOT EXISTS music_track (
+	music_track_id INT PRIMARY KEY,
+	track_name VARCHAR(60) NOT NULL,
+	track_duration INT NOT NULL,
+	fk_album_id INT REFERENCES album(album_id) NOT NULL
 );
 
-create table if not exists playlist (
-	playlist_id int primary key,
-	playlist_title text not null,
-	year_of_playlist date not null,
-	fk_music_track_id int references music_track(music_track_id) not null
+CREATE TABLE IF NOT EXISTS playlist (
+	playlist_id INT PRIMARY KEY,
+	playlist_title VARCHAR(60) NOT NULL,
+	year_of_playlist DATE NOT NULL
 );
 
-create table if not exists album_performer (
-	album_id int references album(album_id),
-	performer_id int references performer(performer_id),
-	constraint album_performer_pk primary key(album_id, performer_id)
+CREATE TABLE IF NOT EXISTS playlist_music_track (
+	playlist_id INT REFERENCES playlist(playlist_id),
+	music_track_id INT REFERENCES music_track(music_track_id),
+	CONSTRAINT playlist_music_track_pk PRIMARY KEY(playlist_id, music_track_id)
 );
 
-create table if not exists performer_musical_ganre (
-	performer_id int references performer(performer_id),
-	musical_ganre_id int references musical_ganre(musical_ganre_id),
-	constraint performer_musical_ganre_pk primary key(performer_id, musical_ganre_id)
+CREATE TABLE IF NOT EXISTS album_performer (
+	album_id INT REFERENCES album(album_id),
+	performer_id INT REFERENCES performer(performer_id),
+	CONSTRAINT album_performer_pk PRIMARY KEY(album_id, performer_id)
+);
+
+CREATE TABLE IF NOT EXISTS performer_musical_ganre (
+	performer_id INT REFERENCES performer(performer_id),
+	musical_ganre_id INT REFERENCES musical_ganre(musical_ganre_id),
+	CONSTRAINT performer_musical_ganre_pk PRIMARY KEY(performer_id, musical_ganre_id)
 );
 
 
